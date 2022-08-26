d322b6c84ee88a7ac415e708048d19c427d8c6d3
sne_syn <sss@anadeainc.com>
2022-08-26 15:12:01 +0200

    Setup husky & lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,8 @@ const Home: NextPage = () => {
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
+        <button autoFocus></button>
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
