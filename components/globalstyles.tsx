e86682c9a3db16d07769a1e581640d52ce238461
sne_syn <sss@anadeainc.com>
2022-08-28 15:35:20 +0200

    Interpolate reset into styled component

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset};
   html,
   body {
     color: ${({ theme }) => theme.colors.primary};
